b4a7e173584df1a8c3e663969cba05fe12717f95
Babrica <mixsaywerri@gmail.com>
2023-02-14 16:53:20 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -2,43 +2,69 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        brand: "Vue Mastery",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 100,
         onSale: true,
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        selectedVariant: 0,
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         cart: 0,
-
-
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
-
     },
-    methods:{
-        addToCart(){
-            this.cart +=1
+    methods: {
+        addToCart() {
+            this.cart += 1
         },
-        deleteCart(){
-            if(this.cart>0){
-            this.cart -=1}
-        }
+        deleteCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
     },
+
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale()
+        {
+            if(this.onSale == true)
+            {
+                return 'Проходит распродажа ' + this.brand + ' ' + this.product;
+            }
+            else
+            {
+                return 'Скидки нет ;( ' + this.brand + ' ' + this.product;
+            }
+
+        },
+    }
 })
 
 
