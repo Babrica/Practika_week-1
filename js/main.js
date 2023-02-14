59b055f1cea39ff93b77aa91be561c98275f77b3
Babrica <mixsaywerri@gmail.com>
2023-02-14 15:21:40 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 100,
         onSale: true,
-        inStock: true,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         variants: [
             {
                 variantId: 2234,
@@ -22,23 +23,22 @@ let app = new Vue({
             }
         ],
         cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1
-            }
-        },
+
+
         updateProduct(variantImage) {
             this.image = variantImage
         },
-        methods: {
-            addToCart() {
-                this.cart -=1
-            }
+
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        deleteCart(){
+            if(this.cart>0){
+            this.cart -=1}
         }
-
-
-    }
-
+    },
 })
 
 
